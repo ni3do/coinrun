d217203a94d6e4ead3bf5d5107ead814a116a83f
niedo <57731234+ni3do@users.noreply.github.com>
2023-05-10 09:40:37 +0200

    adjusted venv path

--- scripts/eps.sh
+++ scripts/eps.sh
@@ -34,7 +34,7 @@ echo "SLURM_JOB_ID:    ${SLURM_JOB_ID}"
 # Binary or script to execute
 
 cd $HOME/coinrun/coinrun
-$HOME/venv/bin/python3 -m coinrun.train_agent --run-id eps --num-levels 500 --epsilon-greedy 0.05
+$HOME/coinrun/venv/bin/python3 -m coinrun.train_agent --run-id eps --num-levels 500 --epsilon-greedy 0.05
 
 echo "Finished at:     $(date)"
 
