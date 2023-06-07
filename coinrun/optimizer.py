dd9688b2a85f194393aacb9618d8232afdde184b
niedo <57731234+ni3do@users.noreply.github.com>
2023-06-07 18:03:44 +0200

    running with random seed, adjusted for new model_stats setup

--- coinrun/optimizer.py
+++ coinrun/optimizer.py
@@ -1,5 +1,8 @@
+import os
+
 import optuna
 import pandas as pd
+from optuna.samplers import RandomSampler
 
 
 def create_bash_script(l2, dropout, epsilon):
@@ -27,45 +30,62 @@ def create_bash_script(l2, dropout, epsilon):
         f.write(base_1)
 
 
-def get_trials():
-    trials = []
+# def get_trials():
+#     trials = []
 
-    df = pd.read_csv("model_stats.csv")
-    for index, row in df.iterrows():
-        trials.append(
-            optuna.trial.create_trial(
-                params={"dropout": row["dropout"], "l2": row["l2"], "epsilon": row["epsilon"]},
-                distributions={
-                    "dropout": optuna.distributions.FloatDistribution(0.0001, 0.1),
-                    "l2": optuna.distributions.FloatDistribution(0.00001, 0.001),
-                    "epsilon": optuna.distributions.FloatDistribution(0.0001, 0.1),
-                },
-                value=row["test_score"],
-            )
-        )
-
-    return trials
+#     df = pd.read_csv("model_stats.csv")
+#     for index, row in df.iterrows():
+#         trials.append(
+#             optuna.trial.create_trial(
+#                 params={
+#                     "dropout": row["dropout"],
+#                     "l2": row["l2"],
+#                     "epsilon": row["epsilon"],
+#                 },
+#                 distributions={
+#                     "dropout": optuna.distributions.FloatDistribution(0.0001, 0.1),
+#                     "l2": optuna.distributions.FloatDistribution(0.00001, 0.001),
+#                     "epsilon": optuna.distributions.FloatDistribution(0.0001, 0.1),
+#                 },
+#                 value=row["test_score"],
+#             )
+#         )
+#     print(f"Loaded {len(trials)} trials")
+#     return trials
 
 
 def objective(trial):
-    dropout = trial.suggest_float("dropout", 0.0001, 0.1)
-    l2 = trial.suggest_float("l2", 0.00001, 0.001)
-    epsilon = trial.suggest_float("epsilon", 0.0001, 0.1)
+    dropout = round(trial.suggest_float("dropout", 0.0001, 0.1), 16)
+    l2 = round(trial.suggest_float("l2", 0.00001, 0.001), 16)
+    epsilon = round(trial.suggest_float("epsilon", 0.0001, 0.1), 16)
 
     df = pd.read_csv("model_stats.csv")
 
-    rows = df.loc[(df["l2"] == l2) & (df["dropout"] == dropout) & (df["epsilon"] == epsilon)]
+    rows = df.loc[
+        (df["l2"] == l2) & (df["dropout"] == dropout) & (df["epsilon"] == epsilon)
+    ]
     if len(rows) == 0:
         print(f"New model needed")
         print(f"dropout: {dropout}, l2: {l2}, epsilon: {epsilon}")
         create_bash_script(l2, dropout, epsilon)
-        raise Exception(f"New model needed with: dropout: {dropout}, l2: {l2}, epsilon: {epsilon}")
+        print(f"Running script")
+
+        raise Exception(
+            f"New model needed with: dropout: {dropout}, l2: {l2}, epsilon: {epsilon}"
+        )
 
     else:
         print(f"Model already exists")
-        return rows["test_score"]
+
+        test_scores = eval(
+            df.loc[
+                (df["l2"] == l2)
+                & (df["dropout"] == dropout)
+                & (df["epsilon"] == epsilon)
+            ]["test_score"].loc[0]
+        )
+        return test_scores[-1]
 
 
-study = optuna.create_study(direction="maximize")
-study.add_trials(get_trials())
-study.optimize(objective, n_trials=100)
+study = optuna.create_study(sampler=RandomSampler(1337), direction="maximize")
+study.optimize(objective, n_trials=10)
